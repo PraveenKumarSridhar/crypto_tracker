d2d98e568a9c0b15a2c0164de7d3a2de2f004d46
Praveen Kumar Sridhar <prasri.pk@gmail.com>
2022-01-01 15:49:11 -0500

    basic bit coin tracker

---
+++ src/basic_tracker.py
@@ -0,0 +1,39 @@
+import pandas as pd
+import numpy as np
+import yfinance as yf
+
+import plotly.graph_objects as go
+
+data = yf.download(tickers = 'BTC-USD',period = '22h',interval = '15m')
+
+#declare figure
+fig = go.Figure()
+
+#Candlestick
+fig.add_trace(go.Candlestick(x=data.index,
+                open=data['Open'],
+                high=data['High'],
+                low=data['Low'],
+                close=data['Close'], name = 'market data'))
+
+# Add titles
+fig.update_layout(
+    title='Bitcoin live share price evolution',
+    yaxis_title='Bitcoin Price (kUS Dollars)')
+
+# X-Axes
+fig.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=15, label="15m", step="minute", stepmode="backward"),
+            dict(count=45, label="45m", step="minute", stepmode="backward"),
+            dict(count=1, label="HTD", step="hour", stepmode="todate"),
+            dict(count=6, label="6h", step="hour", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
+)
+
+#Show
+fig.show()  
